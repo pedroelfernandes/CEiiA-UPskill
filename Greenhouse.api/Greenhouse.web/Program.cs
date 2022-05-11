0e02ac104842315d2a6bffa05bfe8c631cc72b75
APL1234 <annapereraleg@gmail.com>
2022-05-11 15:45:02 +0100

    With EntityFramework Nuggets

--- Greenhouse.api/Greenhouse.web/Program.cs
+++ Greenhouse.api/Greenhouse.web/Program.cs
@@ -1,7 +1,17 @@
+using Greenhouse.web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//configure data and connection string
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(
+    builder.Configuration.GetConnectionString("GreenHouseDBConnection")
+    )
+);
 
 var app = builder.Build();
 
