db7a36189341cd0415f81bf998fb624cc4266069
gbmat0s <gustavobmatos21@gmail.com>
2022-05-12 16:32:03 +0100

    Added second layer API

---
+++ Greenhouse.api/MainAPI/Models/API.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MainAPI.Models
+{
+    public class API
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+
+        [StringLength(25)]
+        public string? Name { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Url)]
+        public string? Url { get; set; }
+    }
+}
---
+++ Greenhouse.api/MainAPI/Models/Reading.cs
@@ -0,0 +1,19 @@
+﻿namespace MainAPI.Models
+{
+    public class Reading
+    {
+        public string? Id { get; set; }
+
+
+        public string? SensorId { get; set; }
+
+
+        public string? SensorType { get; set; }
+
+
+        public DateTime? ReadDate { get; set; }
+
+
+        public string? Value { get; set; }
+    }
+}
