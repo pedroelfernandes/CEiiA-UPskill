bea362d6283aeec5325ffad38064f12d6710c143
Pedro Fernandes <pedroelfernandes@hotmail.com>
2022-05-17 11:43:21 +0100

    Added DTO architecture to WeatherStation.API

---
+++ Greenhouse.api/WeatherStation.API/Repositories/ReadingRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using WeatherStation.api.Models;
+
+namespace WeatherStation.api.Repositories
+{
+    public class ReadingRepository
+    {
+        private readonly IMongoCollection<Reading> _readingsCollection;
+
+
+        public ReadingRepository(IOptions<WeatherStationDatabaseSettings> options)
+        {
+            MongoClient mongoClient = new MongoClient(options.Value.ConnectionString);
+
+            IMongoDatabase mongoDatabase = mongoClient.GetDatabase(options.Value.DatabaseName);
+
+            _readingsCollection = mongoDatabase.GetCollection<Reading>(options.Value.Readings);
+        }
+
+
+        public async Task<List<Reading>> GetAsync() =>
+            await _readingsCollection.Find(_ => true).ToListAsync();
+
+
+        public List<Reading> GetLast()
+        {
+            var myList = _readingsCollection.AsQueryable().OrderByDescending(r => r.ReadDate).ToList().DistinctBy(r => r.SensorId).ToList();
+            return myList;
+        }
+
+        public async Task<Reading> GetLastBySensorId(string id) =>
+            await _readingsCollection.Find(r => r.SensorId == id).SortByDescending(r => r.ReadDate).FirstOrDefaultAsync();
+
+        public async Task<List<Reading>> GetLastValuesBySensorId(string id, int limit) =>
+            await _readingsCollection.Find(r => r.SensorId == id).SortByDescending(r => r.ReadDate).Limit(limit).ToListAsync();
+    }
+}
