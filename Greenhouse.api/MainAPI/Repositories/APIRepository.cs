308bf1ea5b9760ad00ab81bcfc91d6b4a30a8c22
gbmat0s <gustavobmatos21@gmail.com>
2022-05-20 12:01:02 +0100

    fixed MainAPI dependency issues

--- Greenhouse.api/MainAPI/Repositories/APIRepository.cs
+++ Greenhouse.api/MainAPI/Repositories/APIRepository.cs
@@ -5,16 +5,7 @@ namespace MainAPI.Repositories
 {
     public class APIRepository
     {
-        private readonly ApplicationDbContext _db;
-
-
-        public APIRepository(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-
-
-        public List<API> GetAPIs() =>
-            _db.APIs.ToList();
+        public static List<API> GetAPIs(ApplicationDbContext db) =>
+            db.APIs.ToList();
     }
 }
