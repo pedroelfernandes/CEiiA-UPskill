c34785855bc16e106ed06d18ea186b0ddf7e70da
Pedro Fernandes <pedroelfernandes@hotmail.com>
2022-05-23 12:56:11 +0100

    fixed some naming issues

--- Greenhouse.api/MainAPI/DTO/SensorDTO.cs
+++ Greenhouse.api/MainAPI/DTO/SensorDTO.cs
@@ -4,18 +4,18 @@ namespace MainAPI.DTO
 {
     public class SensorDTO
     {
-        public string? SensorId { get; set; }
+        public string? Id { get; set; }
 
 
-        public string? SensorType { get; set; }
+        public string? Type { get; set; }
 
 
         public static SensorDTO ToDto(Reading reading)
         {
             return new SensorDTO()
             {
-                SensorId = reading.SensorId,
-                SensorType = reading.SensorType
+                Id = reading.SensorId,
+                Type = reading.SensorType
             };
         }
     }
