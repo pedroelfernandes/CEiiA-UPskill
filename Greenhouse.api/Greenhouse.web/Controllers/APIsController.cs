9e59e98f20b8a1662de40022f843f5201939252c
APL1234 <annapereraleg@gmail.com>
2022-05-26 12:54:15 +0100

    Method Get APIs

--- Greenhouse.api/Greenhouse.web/Controllers/APIsController.cs
+++ Greenhouse.api/Greenhouse.web/Controllers/APIsController.cs
@@ -7,125 +7,128 @@ using Microsoft.Extensions.Configuration;
 
 namespace Greenhouse.web.Controllers
 {
+    [Route("api/[action]")]
     public class APIsController : Controller
     {
        
-        IConfiguration _configuration;
+        private readonly IConfiguration _configuration;
         public APIsController(IConfiguration configuration)
         {
           _configuration = configuration;
         }
 
-        //DISPLAY API
-        public IActionResult Index()
-        {
-            var aPIs = APIServices.GetAPI(_configuration).Result;
 
-            return View(aPIs);
+        //GetAPIs
+
+        public async Task<IActionResult> GetAPIs()
+        {
+            //ViewBag.apiId = apiId;
+            return View(await APIServices.GetAPI(_configuration));
+
         }
 
-        //TEST_SIMILAR TO MAIN
-        [HttpGet]
-        public async Task<List<API>> GetAPIs() =>
-            (List<API>)await APIServices.GetAPI(_configuration);
 
-        ////DISPLAY API
-        //public IActionResult Index()
+        //////DISPLAY API
+        ////public IActionResult Index()
+        ////{
+        ////    string uri = _configuration.GetValue<string>("URI");
+        ////    var aPIs = API.GetAPIs(uri).Result;
+        ////    string uri = _configuration.GetValue<string>(ClientServices.GetAPI());
+
+
+        ////    return View(aPIs);
+        ////}
+
+
+        ////ADD function (CREATE)
+        //public IActionResult Create()
         //{
-        //    string uri = _configuration.GetValue<string>("URI");
-        //    var aPIs = API.GetAPIs(uri).Result;
-        //    string uri = _configuration.GetValue<string>(ClientServices.GetAPI());
-
-
-        //    return View(aPIs);
+        //    return View();
         //}
 
-        //ADD function (CREATE)
-        public IActionResult Create()
-        {
-            return View();
-        }
 
-        //ADD function (CREATE-POST to Index)
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        ////ADD function (CREATE-POST to Index)
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
 
-        public IActionResult Create(API aPI)
-        {
-
-            //validate the model rules
-            if (!ModelState.IsValid)
-                return View(aPI);
+        //public IActionResult Create(API aPI)
+        //{
+        //    //validate the model rules
+        //    if (!ModelState.IsValid)
+        //        return View(aPI);
+        //    return RedirectToAction("Index");
+        //}
 
 
-          
-            return RedirectToAction("Index");
-        }
+        ////method EDIT (by id)
+        //public IActionResult Edit(int? id)
+        //{
 
-        //method EDIT (by id)
-        public IActionResult Edit(int? id)
-        {
+        //    API aPI = new();
 
-            API aPI = new();
+        //    if (id is null)
+        //        return BadRequest();
 
-            if (id is null)
-                return BadRequest();
+        //    if (id <= 0)
+        //        return NotFound();
 
-            if (id <= 0)
-                return NotFound();
+        //    if (aPI == null)
+        //        return NotFound();
 
-            if (aPI == null)
-                return NotFound();
-
-            return View(aPI);
-        }
-
-        //Method EDIT - Post (handle the new input)
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int apiId, API aPI)
-        {
-
-            //validate the model rules
-            if (!ModelState.IsValid)
-                return View(aPI);
-
-            //Return redirect to action
-            return RedirectToAction("Index");
-        }
-
-        //DELETE
-        public IActionResult Delete(int? id)
-        {
-            
-            
-           API aPI = new();
-
-            if (id is null)
-                return BadRequest();
-
-            if (id <= 0)
-                return NotFound();
-
-            if (aPI == null)
-                return NotFound();
-
-            return View(aPI);
-        }
-
-        //HTTP Method: Post (handle the new input)
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int? id)
-        {
-            API aPI = new();
-
-            if (aPI == null)
-                return NotFound();
-
-            return RedirectToAction("Index");
+        //    return View(aPI);
+        //}
 
 
-        }
+        ////Method EDIT - Post (handle the new input)
+        ///
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+
+        //public IActionResult Edit(int apiId, API aPI)
+        //{
+
+        //    //validate the model rules
+        //    if (!ModelState.IsValid)
+        //        return View(aPI);
+
+        //    //Return redirect to action
+        //    return RedirectToAction("Index");
+        //}
+
+
+        ////DELETE
+        //public IActionResult Delete(int? id)
+        //{
+        //   API aPI = new();
+
+        //    if (id is null)
+        //        return BadRequest();
+
+        //    if (id <= 0)
+        //        return NotFound();
+
+        //    if (aPI == null)
+        //        return NotFound();
+
+        //    return View(aPI);
+        //}
+
+
+
+        ////HTTP Method: Post (handle the new input)
+        ///
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+
+        //public IActionResult DeletePOST(int? id)
+        //{
+        //    API aPI = new();
+
+        //    if (aPI == null)
+        //        return NotFound();
+
+        //    return RedirectToAction("Index");
+
+        //}
     }
 }
--- Greenhouse.api/Greenhouse.web/Models/API.cs
+++ Greenhouse.api/Greenhouse.web/Models/API.cs
@@ -17,58 +17,6 @@ namespace Greenhouse.web.Models
 
         [StringLength(250)]
         public string Description { get; set; } = null!;
-
-        //public static async Task<IEnumerable<API>> GetAPIs(string uri)
-        //{
-        //    IEnumerable<API> apis = new List<API>();
-
-        //    HttpClient client = Helpers.Helpers.GetHttpClient(uri);
-
-        //    HttpResponseMessage response = await client.GetAsync("Main/GetAPIs");
-
-        //    response.EnsureSuccessStatusCode();
-
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        var res = await response.Content.ReadFromJsonAsync<List<API>>();
-
-        //        if (res != null)
-        //        {
-        //            apis = res;
-        //        }
-        //    }
-
-
-        //    return apis;
-
-        
-        //    public static async Task<API> CreateAPI(string uri, API api)
-        //{
-
-
-        //    HttpClient client = Helpers.Helpers.GetHttpClient(uri);
-
-        //    HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(api), Encoding.UTF8);
-        //    httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-        //    var response = await client.PostAsync(uri + "Main/GetAPIs", httpContent);
-
-        //    response.EnsureSuccessStatusCode();
-
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        var res = JsonConvert.DeserializeObject<API>(await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync());
-
-
-        //        if (res != null)
-        //        {
-        //            api = res;
-        //        }
-        //    }
-
-        //    return api;
-        //}
-
     }
 }
 
