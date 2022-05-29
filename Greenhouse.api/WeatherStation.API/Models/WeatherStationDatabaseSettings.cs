f175adda67790dd555c99a9bbf3771a0f72a7bc6
João Martins <85556776+Vaazch@users.noreply.github.com>
2022-05-30 00:48:39 +0100

    Update WeatherStationDatabaseSettings.cs

--- Greenhouse.api/WeatherStation.API/Models/WeatherStationDatabaseSettings.cs
+++ Greenhouse.api/WeatherStation.API/Models/WeatherStationDatabaseSettings.cs
@@ -2,7 +2,11 @@
 {
     public class WeatherStationDatabaseSettings
     {
+        
+        //this string helps with a better coesion of configuration injection and make's it less prone to human error 
+        
         public const string Name = "WeatherStationDatabase";
+        
         public string ConnectionString { get; set; } = null!;
 
         public string DatabaseName { get; set; } = null!;
