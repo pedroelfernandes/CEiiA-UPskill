992f28f1b535e114db4a98f23304258f59b4a895
JMDias89 <jmrsdias@gmail.com>
2022-05-30 16:43:50 +0100

    Active attribute added to models
    Commented  API class in Sensor.cs because at this point it isn't known weather it'll be needed in the future in the way that API.cs is structured.

--- Greenhouse.api/MainAPI/Models/Role.cs
+++ Greenhouse.api/MainAPI/Models/Role.cs
@@ -11,5 +11,8 @@ namespace MainAPI.Models
         public string? Name { get; set; }
 
         public string? Description { get; set; }
+
+        [Required]
+        public bool Active { get; set; }
     }
 }
