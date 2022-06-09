09efbafc85747ade2b3114f40ddf6fab290b8094
APL1234 <annapereraleg@gmail.com>
2022-06-09 10:55:03 +0100

    Sensors-Asset-AssetTypes VIEWS

---
+++ Greenhouse.api/Greenhouse.web/Controllers/SensorTypesController.cs
@@ -0,0 +1,27 @@
+﻿using Greenhouse.web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greenhouse.web.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class SensorTypesController : Controller
+    {
+
+        private readonly ISensorTypeServices _sensorTypeServices;
+        public SensorTypesController(ISensorTypeServices sensorTypeServices)
+        {
+            _sensorTypeServices = sensorTypeServices;
+        }
+
+        //public async Task<IEnumerable<AssetType>> GetAssetTypes() => await _assetTypeServices.GetAssetTypes();
+
+        //Get Full List of AssetTypes
+
+        [HttpGet]
+        public async Task<IActionResult> GetSensorTypes()
+        {
+            return View(await _sensorTypeServices.GetSensorTypes());
+        }
+
+    }
+}
