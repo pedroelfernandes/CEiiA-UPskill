fdb90896614459d1f27cc1e4b024bb771f2dc7aa
gbmat0s <gustavobmatos21@gmail.com>
2022-06-20 10:48:47 +0100

    first draft of dashboard

---
+++ Greenhouse.api/Greenhouse.web/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Greenhouse.web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greenhouse.web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IAssetServices _assetServices;
+
+
+        public DashboardController(IAssetServices assetServices)
+        {
+            _assetServices = assetServices;
+        }
+
+
+        public async Task<IActionResult> Index() =>
+            View(await _assetServices.GetAssets());
+    }
+}
