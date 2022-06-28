0a8b30cda063346590c9dac8ae6cab86de52b355
APL1234 <annapereraleg@gmail.com>
2022-06-28 16:21:54 +0100

    HightlighActiveRowsInLists_CLIENT_working

--- Greenhouse.api/Greenhouse.web/wwwroot/js/site.js
+++ Greenhouse.api/Greenhouse.web/wwwroot/js/site.js
@@ -3,33 +3,3 @@
 
 // Write your JavaScript code.
 
-
-var rows = document.getElementById("trans_separate").getElementsByTagName("tbody")
-[0].getElementsByTagName("tr");
-
-// loops through each row
-for (i = 0; i < rows.length; i++) {
-    cells = rows[i].getElementsByTagName('td');
-    if (cells[0].innerHTML == 'R')
-        rows[i].className = "red";
-
-    if (cells[0].innerHTML == 'Y')
-        rows[i].className = "yellow";
-}
-
-//Password confirmation
-
-//var password = document.getElementById("password")
-//var confirm_password = document.getElementById("oldPassword");
-
-//function validatePassword(){
-//    if (password.value != oldPassword.value) {
-//        confirm_password.setCustomValidity("Passwords Don't Match");
-//    } else {
-//        confirm_password.setCustomValidity('');
-//    }
-//}
-
-//password.onchange = validatePassword;
-//confirm_password.onkeyup = validatePassword;
-
